13e128caa6700d0c107e541d4548f25298d495a2
kkamara <itchykilljoy@protonmail.com>
2018-07-15 11:04:58 +0100

    database schema changes

---
+++ database/migrations/2018_07_15_095812_delete_order_history_table_product_id_column.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeleteOrderHistoryTableProductIdColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_history', function($table) {
+            $table->dropColumn('product_id');
+        });
+    }
+}
