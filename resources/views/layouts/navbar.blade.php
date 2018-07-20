60678263697bd5ce64cd9cc942bc700196f52a4e
kkamara <itchykilljoy@protonmail.com>
2018-07-20 19:05:52 +0100

    navbar changes

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
+        <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,25 +8,14 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
+                <div class="nav-item">
+                    <a class="nav-link" href="#">Most Popular</a>
+                </div>
+                <div class="nav-item">
+                    <a class="nav-link" href="#">Top Rated</a>
+                </div>
             </ul>
             <ul class="navbar-nav mr-auto">
                 <form class="form-inline my-2 my-lg-0">
@@ -35,6 +24,19 @@
                 </form>
             </ul>
             <ul class="navbar-nav mr-right">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        My Stuff
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Billing Cards</a>
+                        <a class="dropdown-item" href="#">Addresses</a>
+                        <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Order History</a>
+                        <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">User Settings</a>
+                    </div>
+                </li>
                 @if(Auth::check())
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('logout') }}">Logout</a>
