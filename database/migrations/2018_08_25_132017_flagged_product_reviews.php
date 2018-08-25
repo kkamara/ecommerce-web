ccf4fd91327b480d77a7deeece5ff6754f2c0355
kkamara <itchykilljoy@protonmail.com>
2018-08-25 14:25:27 +0100

    added flagged product reviews migration

---
+++ database/migrations/2018_08_25_132017_flagged_product_reviews.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FlaggedProductReviews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flagged_product_reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_reviews_id')
+                  ->references('id')->on('product_reviews')
+                  ->onDelete('cascade');
+            $table->integer('flagged_count')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flagged_product_reviews');
+    }
+}
