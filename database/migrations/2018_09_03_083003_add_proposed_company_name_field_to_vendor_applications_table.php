c363c4ed088751619296f58a5ec20742fd935105
kkamara <itchykilljoy@protonmail.com>
2018-09-03 09:37:56 +0100

    added migrations for vendor_applications table

---
+++ database/migrations/2018_09_03_083003_add_proposed_company_name_field_to_vendor_applications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProposedCompanyNameFieldToVendorApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vendor_applications', function (Blueprint $table) {
+            $table->string('proposed_company_name')->unique()->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vendor_applications', function (Blueprint $table) {
+            $table->dropColumn('proposed_company_name');
+        });
+    }
+}
