6fec0c749cdd12de0e0cc9cf24584cd350176f9e
kkamara <itchykilljoy@protonmail.com>
2019-07-12 15:52:58 +0100

    server allow cross-origin requests

--- server/app/Providers/RouteServiceProvider.php
+++ server/app/Providers/RouteServiceProvider.php
@@ -66,7 +66,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-             ->middleware('api')
+             ->middleware(['api', 'cors'])
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
