2e659f1c9845ee18a613a0fb0716a584abc1ac68
kkamara <itchykilljoy@protonmail.com>
2019-08-11 15:37:20 +0100

    server: fixed character issue in app config

--- config/app.php
+++ config/app.php
@@ -172,7 +172,7 @@ return [
 
         /*
          * Package Service Providers...
-         */
+         */
         Spatie\Permission\PermissionServiceProvider::class,
         Laravel\Dusk\DuskServiceProvider::class,
 
