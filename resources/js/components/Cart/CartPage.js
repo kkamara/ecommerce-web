b980499a080420eeeac3c98bf057d724632fa69a
kkamara <kelvinkamara@protonmail.com>
2020-08-18 00:32:21 +0100

    Fix cart page design

--- resources/js/components/Cart/CartPage.js
+++ resources/js/components/Cart/CartPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 
 import Loader from "../Loader";
@@ -8,6 +8,71 @@ const CartPage = ({ cart: cartObj }) => {
     console.log("cart", "isLoaded", "fetched", cart, isLoaded, fetched);
 
     const updateCart = e => {};
+
+    const _renderCartForm = () => {
+        return (
+            <form action="" onSubmit={e => updateCart(e)}>
+                {cart.items && cart.items.length ?
+                    (
+                        <Fragment>
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Item</th>
+                                        <th>Price</th>
+                                        <th>Amount</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {cart.items.map(({
+                                        product,
+                                        amount,
+                                    }, index) => (
+                                        <tr key={index}>
+                                            <td>
+                                                <a
+                                                    href={`/products/${product.id}`}
+                                                >
+                                                    {product.name}
+                                                </a>
+                                            </td>
+
+                                            <td>{product.formatted_cost}</td>
+
+                                            <td>
+                                                <input
+                                                    className="form-control"
+                                                    type="number"
+                                                    name={`amount-${product.id}`}
+                                                    defaultValue={`${amount}`}
+                                                />
+                                            </td>
+                                        </tr>
+                                    ))}
+                                    <tr>
+                                        <th>Total:</th>
+                                        <td>{cart.price}</td>
+                                        <td>{cart.count}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <br />
+                            <div className="pull-right">
+                                <input
+                                    type="submit"
+                                    href=""
+                                    className="btn btn-primary"
+                                    defaultValue="Update details"
+                                />
+                            </div>
+                        </Fragment>
+                     ) : (
+                        <p>Your cart is empty.</p>
+                     )
+                }
+            </form>
+        );
+    };
 
     if (!isLoaded) {
         return <Loader />;
@@ -22,68 +87,16 @@ const CartPage = ({ cart: cartObj }) => {
                     <h3 className="lead">
                         <strong>Your Cart</strong>
                     </h3>
-                    {/* @if(! empty($cart)) */}
-                    <form action="" onSubmit={e => updateCart(e)}>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>Item</th>
-                                    <th>Price</th>
-                                    <th>Amount</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {cart.items.map((item, index) => (
-                                    <tr key={index}>
-                                        <td>
-                                            <a
-                                                href={`/products/${item.product.id}`}
-                                            >
-                                                {item.product.name}
-                                            </a>
-                                        </td>
-
-                                        <td>{item.product.formatted_cost}</td>
-
-                                        <td>
-                                            <input
-                                                className="form-control"
-                                                type="number"
-                                                name={`amount-${item.product.id}`}
-                                                defaultValue={`${item.amount}`}
-                                            />
-                                        </td>
-                                    </tr>
-                                ))}
-                                <tr>
-                                    <th>Total:</th>
-                                    <td>{cart.price}</td>
-                                    <td>{cart.count}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <br />
-                        <div className="pull-right">
-                            <input
-                                type="submit"
-                                href=""
-                                className="btn btn-primary"
-                                defaultValue="Update details"
-                            />
-                        </div>
-                        {/* @else
-                    <p>Your cart is empty.</p>
-                    @endif */}
-                    </form>
-                    <div className="col-md-4">
-                        <ul className="list-group">
-                            <li className="list-group-item">
-                                {/* <a href='{{ route('orderCreate') }}' className='btn btn-success' style='display:block;'>
+                    {_renderCartForm()}
+                </div>
+                <div className="col-md-4">
+                    <ul className="list-group">
+                        <li className="list-group-item">
+                            <a href='#' className='btn btn-success' style={{ display: 'block' }}>
                                 Proceed to checkout
-                            </a> */}
-                            </li>
-                        </ul>
-                    </div>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
