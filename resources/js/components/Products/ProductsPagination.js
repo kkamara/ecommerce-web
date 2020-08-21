4783db2ac4b398468dd112c7538a20757cdecdf9
kkamara <kelvinkamara@protonmail.com>
2020-08-21 11:47:58 +0100

    Fix pagination ui (url href & active tab)

--- resources/js/components/Products/ProductsPagination.js
+++ resources/js/components/Products/ProductsPagination.js
@@ -2,28 +2,25 @@ import React from "react";
 import { connect } from "react-redux";
 import Pagination from "react-js-pagination";
 
-class ProductsPagination extends React.Component {
-    render() {
-        const { handlePageChange, activePage, products } = this.props;
-
-        if (products.products.products.data.length) {
-            return (
-                <div>
-                    <Pagination
-                        innerClass="pagination pagination-lg"
-                        itemClass="page-item"
-                        linkClass="page-link"
-                        activePage={activePage}
-                        itemsCountPerPage={products.products.products.per_page}
-                        totalItemsCount={products.products.products.total}
-                        pageRangeDisplayed={7}
-                        onChange={handlePageChange}
-                    />
-                </div>
-            );
-        } else {
-            return <div />;
-        }
+const ProductsPagination = ({ handlePageChange, products }) => {
+    if (products.products.products.data.length) {
+        return (
+            <div>
+                <Pagination
+                    innerClass="pagination pagination-lg"
+                    itemClass="page-item"
+                    linkClass="page-link"
+                    activePage={products.products.activePage}
+                    itemsCountPerPage={products.products.products.per_page}
+                    totalItemsCount={products.products.products.total}
+                    pageRangeDisplayed={7}
+                    onChange={handlePageChange}
+                    getPageUrl={pageNumber => `${pageNumber}`}
+                />
+            </div>
+        );
+    } else {
+        return <div />;
     }
 }
 
