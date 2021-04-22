2b38a343f58111b52381da19b82c3e6b1553ec66
kkamara <kelvinkamara@protonmail.com>
2021-04-22 15:25:05 +0100

    remove laravel app

--- resources/views/layouts/errors.blade.php
+++
@@ -1,7 +0,0 @@
-@if(!empty($errors))
-<ul class="list-group errors" style='list-style-type:none;margin:0px 0px 10px 0px;'>
-    @foreach($errors as $error)
-    <li style="padding:5px 0px 5px 10px;" class="list-group-item-danger">{{ $error }}</li>
-    @endforeach
-</ul>
-@endif
