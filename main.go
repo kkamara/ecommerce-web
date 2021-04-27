6a51afe185e863a138b6eafa718a13137c6173b4
kkamara <kelvinkamara@protonmail.com>
2021-04-27 20:59:48 +0100

    Add view templates

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/kkamara/laravel-ecommerce/engine"
 )
 
 func main() {
-	app := *fiber.New()
+	app := *fiber.New(fiber.Config{
+		Views: engine.GetEngine(),
+	})
 
 	app.Use(logger.New())
+
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		}, "layouts/master")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 
 	app.Listen(":3000")
 }
