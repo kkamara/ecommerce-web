3c1f5489d9faf6cb35a180a348d5dc6c69e77a5c
kkamara <kelvinkamara@protonmail.com>
2021-04-30 12:45:37 +0100

    Add user model with tests

---
+++ models/user/user_test.go
@@ -0,0 +1,77 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/bxcodec/faker/v3"
+	"github.com/kkamara/laravel-ecommerce/models/helper"
+	"github.com/kkamara/laravel-ecommerce/schemas"
+)
+
+func TestGetAllUsers(t *testing.T) {
+	password, err := helper.HashPassword("secret")
+	if err != nil {
+		return
+	}
+	u := &schemas.User{
+		FirstName: faker.FirstName(),
+		LastName:  faker.LastName(),
+		Email:     faker.Email(),
+		Password:  password,
+		Role:      "",
+	}
+	user, err := Create(u)
+	if err != nil {
+		return
+	}
+	users, err := GetAll()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(users) == 0 {
+		t.Error("response users has no data")
+	} else {
+		var exists bool
+		for _, respUser := range users {
+			if respUser.Email == user.Email {
+				exists = true
+			}
+		}
+		if !exists {
+			t.Errorf(
+				"response users is missing user %s",
+				user.Email,
+			)
+		}
+	}
+}
+
+func TestRandomModeratorRole(t *testing.T) {
+	role := "moderator"
+	user, err := Random(role)
+	if err != nil {
+		t.Error(err)
+	}
+	if user.Role != role {
+		t.Errorf(
+			"response user role %s does not match %s",
+			user.Role,
+			role,
+		)
+	}
+}
+
+func TestRandomVendorRole(t *testing.T) {
+	role := "vendor"
+	user, err := Random(role)
+	if err != nil {
+		t.Error(err)
+	}
+	if user.Role != role {
+		t.Errorf(
+			"response user role %s does not match %s",
+			user.Role,
+			role,
+		)
+	}
+}
