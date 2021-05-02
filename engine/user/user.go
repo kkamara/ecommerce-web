efe094a6bd2e57938c0a67c82714c1881c7e992d
kkamara <kelvinkamara@protonmail.com>
2021-05-02 12:18:20 +0100

    Refactor engine package

---
+++ engine/user/user.go
@@ -0,0 +1,16 @@
+package user
+
+func Role(name string) bool {
+	// to implement -> moderator, vendor
+	return false
+}
+
+func Userauth() bool {
+	// to implement
+	return false
+}
+
+func UserSlug() string {
+	// to implement
+	return ""
+}
