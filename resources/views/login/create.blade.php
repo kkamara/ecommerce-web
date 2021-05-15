615b94abcac692bffe3beed420dcb420f750dfd4
kkamara <kelvinkamara@protonmail.com>
2021-05-15 15:17:26 +0100

    Deploying to heroku
    * Update asset importing for cors
    
    * Move cross-env to dependencies
    
    * Revert "Move cross-env to dependencies"
    
    * This reverts commit f4b092612cc628539216ed9f0d0f54ab66d713b2.
    
    * Add binaries to vc
    
    * Update import paths frontend
    
    * Add guest logins
    
    * Fix login page not submitting
    
    * Add package-lock.json to vc
    
    * build(deps-dev): bump axios from 0.19.2 to 0.21.1
    
    Bumps [axios](https://github.com/axios/axios) from 0.19.2 to 0.21.1.
    - [Release notes](https://github.com/axios/axios/releases)
    - [Changelog](https://github.com/axios/axios/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/axios/axios/compare/v0.19.2...v0.21.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- resources/views/login/create.blade.php
+++ resources/views/login/create.blade.php
@@ -7,30 +7,44 @@
             <div class="card">
                 <div class="card-body">
                     <div class="col-md-4 offset-md-4">
-                        <div class="card-title">
-                            <a href="{{ route('registerHome') }}" class="btn btn-info btn-sm">
-                                Click here to <strong>create an account</strong>
-                            </a>
-                        </div>
                         <form class='form' action="{{ route('loginCreate') }}" method='POST'>
                             {{ csrf_field() }}
-                            {{ method_field('PUT') }}
+                            <div class="card-title">
+                                <a href="{{ route('registerHome') }}" class="btn btn-info btn-sm">
+                                    Click here to <strong>create an account</strong>
+                                </a>
+                            </div>
                             <div class="card-text">
-                                @include('layouts.errors')
+                                {{ csrf_field() }}
+                                {{ method_field('PUT') }}
+                                    @include('layouts.errors')
 
-                                <div class="form-group">
-                                    <div class="form-control">
-                                        <label>Email
-                                            <input type="text" class='form-control' name='email' value="{{ $input['email'] ?? '' }}">
-                                        </label>
+                                    <p>Logins</p>
+                                    <ul>
+                                        @foreach($logins as $role => $login)
+                                            <li>{{$login['email']}}</li>
+                                        @endforeach
+                                    </ul>
+
+                                    <br/>
+
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input
+                                            type="text"
+                                            class='form-control'
+                                            name='email'
+                                            value="{{ $input['email'] ?? '' }}"
+                                        />
                                     </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="form-control">
-                                        {{ csrf_field() }}
-                                        <label>Password
-                                            <input type="password" class='form-control' name='password'>
-                                        </label>
+                                    <div class="form-group">
+                                        <label for="password">Password</label>
+                                        <input
+                                            type="password"
+                                            class='form-control'
+                                            name='password'
+                                            value="secret"
+                                        />
                                     </div>
                                 </div>
                             </div>
@@ -45,4 +59,12 @@
         </div>
     </div>
 
+@stop
+
+@section('scripts')
+    <script>
+        $(document).ready(() => {
+            $('.role-tooltip').tooltip();
+        });
+    </script>
 @stop
