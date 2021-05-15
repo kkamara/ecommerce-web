9c74de201f48054ca1a4cdd6e9996c6a4896061c
kkamara <kelvinkamara@protonmail.com>
2021-05-15 15:17:26 +0100

    Deploying to heroku
    * Update asset importing for cors
    
    * Move cross-env to dependencies
    
    * Revert "Move cross-env to dependencies"
    
    * This reverts commit f4b092612cc628539216ed9f0d0f54ab66d713b2.
    
    * Add binaries to vc
    
    * Update import paths frontend
    
    * Add guest logins
    
    * Fix login page not submitting
    
    * Add package-lock.json to vc
    
    * build(deps-dev): bump axios from 0.19.2 to 0.21.1
    
    Bumps [axios](https://github.com/axios/axios) from 0.19.2 to 0.21.1.
    - [Release notes](https://github.com/axios/axios/releases)
    - [Changelog](https://github.com/axios/axios/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/axios/axios/compare/v0.19.2...v0.21.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,14 +4,15 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- <meta name="csrf-token" content='{{ csrf_token() }}'> --}}
+    <meta name="csrf-token" content='{{ csrf_token() }}'>
 
     <title>{{ $title }} | {{ config('app.name') }}</title>
 
     <!-- Styles -->
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <link rel='stylesheet' href="{{ asset('css/app.css') }}">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel='stylesheet' href="/css/app.css">
     <link rel='stylesheet' href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    @section('styles') @show
 </head>
 <body>
 
@@ -28,5 +29,6 @@
     </div>
 
     @include('layouts.scripts')
+    @section('scripts') @show
 </body>
 </html>
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,3 +1,3 @@
 
-<script src="{{ asset('js/jquery.min.js') }}"></script>
-<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="/js/jquery.min.js"></script>
+<script src="/js/app.js" defer></script>
