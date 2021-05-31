59bd6969033758aa7fe9ad4f9cd178c9bb2ef34c
kkamara <kelvinkamara@protonmail.com>
2021-05-31 11:46:32 +0100

    Remove unused controller methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,55 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-use Validator;
 use App\Models\User;
+use Validator;
 
 class UserController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $slug
-     * @return \Illuminate\Http\Response
-     */
-    public function show($slug)
-    {
     }
 
     /**
@@ -141,16 +100,5 @@ class UserController extends Controller
         {
             return abort(404);
         }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $slug
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($slug)
-    {
-        //
     }
 }
